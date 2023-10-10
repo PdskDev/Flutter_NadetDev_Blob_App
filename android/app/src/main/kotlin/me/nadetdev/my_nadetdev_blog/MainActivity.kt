bd96d9e14d647ad51425e9ce930d1cd6a0031b66
NadetDev <nadetdev@gmail.com>
2023-10-10 21:16:13 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/me/nadetdev/my_nadetdev_blog/MainActivity.kt
@@ -0,0 +1,6 @@
+package me.nadetdev.my_nadetdev_blog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NadetDev Blog',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
